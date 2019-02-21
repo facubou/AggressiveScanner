b880eab13ff94dd764cabd9ebeea3e8d1e558814
Facundo Bouza <facundo.bouza@gmail.com>
2019-02-21 08:06:08 -0300

    New Update
    Recibe IP, escaneo hasta el puerto 445 solo para los hosts activos.

--- PortScanner.py
+++ PortScanner.py
@@ -1,25 +1,24 @@
-import os, sys, platform
 from socket import *
-
 import socket
+
 print("------------Escaneo de Red-------------\n")
-ip = input("Ingresar IP y rango a escanear (ejemplo: 10.10.10.10 255): ")
+ip = input("Ingresar IP y rango a escanear (ejemplo: 10.10.10.0:255): ")
 ipParse = ip.split('.')
 print(ipParse)
-convertir = ipParse[3]
-print (ipParse)
-for i in range(0, 100):
-    convertir = int(convertir) + 1
-    host = ipParse[0] + "." + ipParse[1] + "." + ipParse[2] + "." + str(convertir)
+rangoParse = ipParse[3].split(':')
+rango = int(rangoParse[1]) + 1
+
+for i in range(int(rangoParse[0]), int(rango)):
+    host = ipParse[0] + "." + ipParse[1] + "." + ipParse[2] + "." + str(rangoParse[0])
     response = os.popen("ping -n 1 " + host)
-    for line in response.readlines():
-        #print(line)
-        if ("ttl" in line.lower()):
+    for linea in response.readlines():
+        if ("ttl" in linea.lower()):
             print(host, "está activo")
-            break
-    s = socket.socket(AF_INET, SOCK_STREAM)
-    for i in range(10, 445):
-        conn = s.connect_ex((host, i))
-        if (conn == 0):
-            print(host + ' Puerto: ' + str(i) + ' OPEN')
-            s.close()
+            s = socket.socket(AF_INET, SOCK_STREAM)
+            print("Escaneando puertos de: " + host + "\n")
+            for j in range(10, 445):
+                conn = s.connect_ex((host, j))
+                if (conn == 0):
+                    print(host + ' Puerto: ' + str(j) + ' OPEN \n')
+                    s.close()
+    rangoParse[0] = int(rangoParse[0]) + 1
